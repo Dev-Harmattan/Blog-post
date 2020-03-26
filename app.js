78c961b51fa580c9951cc70664a0949a37ca17f3
Dev-Harmattan <ganiyusodiq059@gmail.com>
2020-03-25 18:24:47 -1200

    add procfile and port

--- app.js
+++ app.js
@@ -104,7 +104,11 @@ app.get("/post/:userId", function(req, res){
   });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
